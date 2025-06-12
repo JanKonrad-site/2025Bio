9c1d1ca84e4a30e39c74b081e597b06f35913c90
pk <jan.konrad11@gmail.com>
2025-06-12 15:12:41 +0200

    Initial commit

---
+++ learn.jankonrad.com/js/testy.js
@@ -0,0 +1,12 @@
+ function checkAnswer(button, correct) {
+    if (correct) {
+      button.classList.remove("btn-outline-secondary");
+      button.classList.add("btn-success");
+    } else {
+      button.classList.remove("btn-outline-secondary");
+      button.classList.add("btn-danger");
+    }
+    button.disabled = true;
+    let siblings = button.parentElement.querySelectorAll("button");
+    siblings.forEach(b => b.disabled = true);
+  }
