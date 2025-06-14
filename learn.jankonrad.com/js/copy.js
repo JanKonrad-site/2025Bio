b7c8d08a96cb05a873a9699eeada404e1af51a15
pk <jan.konrad11@gmail.com>
2025-06-14 10:26:48 +0200

    Update

---
+++ learn.jankonrad.com/js/copy.js
@@ -0,0 +1,62 @@
+
+    function copyToClipboard() {
+      const text = document.getElementById("copy-text").innerText;
+      navigator.clipboard.writeText(text).then(() => {
+        const msg = document.getElementById("copiedMsg");
+        msg.style.display = "block";
+        setTimeout(() => msg.style.display = "none", 2000);
+      }).catch(err => {
+        alert("Nepodařilo se zkopírovat text.");
+        console.error(err);
+      });
+    }
+
+
+    function copyToClipboard213() {
+      const text = document.getElementById("copy-text2-1-3").innerText;
+      navigator.clipboard.writeText(text).then(() => {
+        const msg = document.getElementById("copiedMsg2-1-3");
+        msg.style.display = "block";
+        setTimeout(() => msg.style.display = "none", 2000);
+      }).catch(err => {
+        alert("Nepodařilo se zkopírovat text.");
+        console.error(err);
+      });
+    }
+
+
+    function copyToClipboard215() {
+      const text = document.getElementById("copy-text2-1-5").innerText;
+      navigator.clipboard.writeText(text).then(() => {
+        const msg = document.getElementById("copiedMsg2-1-5");
+        msg.style.display = "block";
+        setTimeout(() => msg.style.display = "none", 2000);
+      }).catch(err => {
+        alert("Nepodařilo se zkopírovat text.");
+        console.error(err);
+      });
+    }
+
+    function copyToClipboard217() {
+      const text = document.getElementById("copy-text2-1-7").innerText;
+      navigator.clipboard.writeText(text).then(() => {
+        const msg = document.getElementById("copiedMsg2-1-7");
+        msg.style.display = "block";
+        setTimeout(() => msg.style.display = "none", 2000);
+      }).catch(err => {
+        alert("Nepodařilo se zkopírovat text.");
+        console.error(err);
+      });
+    }
+
+    function copyToClipboard218() {
+      const text = document.getElementById("copy-text2-1-8").innerText;
+      navigator.clipboard.writeText(text).then(() => {
+        const msg = document.getElementById("copiedMsg2-1-8");
+        msg.style.display = "block";
+        setTimeout(() => msg.style.display = "none", 2000);
+      }).catch(err => {
+        alert("Nepodařilo se zkopírovat text.");
+        console.error(err);
+      });
+    }
